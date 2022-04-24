c311e53f6a5fc6896d5696d9bf0861dcaa972081
cnabilgit <46906074+cnabilhub@users.noreply.github.com>
2022-04-24 21:40:46 +0000

    costumize show

--- resources/views/prospects/index.blade.php
+++ resources/views/prospects/index.blade.php
@@ -10,68 +10,77 @@
 
                 <table class="table-auto min-w-full" id="dataTable">
                     <thead class="border-b bg-gray-800">
-                    <tr>
-                        <th scope="col" class="text-sm font-medium text-white px-2 py-1">
-                            URL
-                        </th>
+                        <tr>
+                            <th scope="col" class="text-sm font-medium text-white px-2 py-1">
+                                URL
+                            </th>
 
-                        <th scope="col" class="text-sm font-medium text-white px-2 py-1">
-                            Name
-                        </th>
+                            <th scope="col" class="text-sm font-medium text-white px-2 py-1">
+                                Name
+                            </th>
 
-                        <th scope="col" class="text-sm font-medium text-white px-2 py-1">
-                            Email
-                        </th>
+                            <th scope="col" class="text-sm font-medium text-white px-2 py-1">
+                                Email
+                            </th>
 
-                        <th scope="col" class="text-sm font-medium text-white px-2 py-1">
-                            Telephone
-                        </th>
+                            <th scope="col" class="text-sm font-medium text-white px-2 py-1">
+                                Telephone
+                            </th>
 
-                        <th scope="col" class="text-sm font-medium text-white px-2 py-1">
-                            Actions
-                        </th>
-                    </tr>
+                            <th scope="col" class="text-sm font-medium text-white px-2 py-1">
+                                Tags
+                            </th>
+
+                            <th scope="col" class="text-sm font-medium text-white px-2 py-1">
+                                Actions
+                            </th>
+                        </tr>
                     </thead>
                     <tbody>
-                    @forelse($prospects as $key =>$prospect)
-                        <tr class="bg-white border-b">
+                        @forelse($prospects as $key =>$prospect)
+                            <tr class="bg-white border-b">
 
-                            <td class="text-sm text-gray-900 font-light px-2 py-1 whitespace-nowrap">
-                                {{$prospect->site_url}}
-                            </td>
+                                <td class="text-sm text-gray-900 font-light px-2 py-1 whitespace-nowrap">
+                                    {{ $prospect->site_url }}
+                                </td>
 
-                            <td class="text-sm text-gray-900 font-light px-2 py-1 whitespace-nowrap">
-                                {{$prospect->name}}
-                            </td>
+                                <td class="text-sm text-gray-900 font-light px-2 py-1 whitespace-nowrap">
+                                    {{ $prospect->name }}
+                                </td>
 
 
-                            <td class="text-sm text-gray-900 font-light px-2 py-1 whitespace-nowrap">
-                                {{$prospect->email}}
-                            </td>
+                                <td class="text-sm text-gray-900 font-light px-2 py-1 whitespace-nowrap">
+                                    {{ $prospect->email }}
+                                </td>
 
-                            <td class="text-sm text-gray-900 font-light px-2 py-1 whitespace-nowrap">
-                                {{$prospect->telephone}}
-                            </td>
+                                <td class="text-sm text-gray-900 font-light px-2 py-1 whitespace-nowrap">
+                                    {{ $prospect->telephone }}
+                                </td>
 
-                            <td class=" text-sm text-gray-900 font-light px-2 py-1 whitespace-nowrap flex">
-                                <a href="{{route('prospects.edit',$prospect->id)}}"
-                                   class="btn-gray">Edit</a>
-                                <form method="POST" action="{{route('prospects.destroy',$prospect->id)}}">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button
-                                        onclick="return confirm('Are you sure')"
-                                        class="ml-2 block btn-primary"> DELETE
-                                    </button>
+                                <td class="text-sm text-gray-900 font-light px-2 py-1 whitespace-nowrap">
+                                    @foreach ($prospect->tags as $tag)
+                                        <span>{{ $tag->name }}</span>
+                                    @endforeach
+                                </td>
 
-                                </form>
-                            </td>
+                                <td class=" text-sm text-gray-900 font-light px-2 py-1 whitespace-nowrap flex">
+                                    <a href="{{ route('prospects.edit', $prospect->id) }}"
+                                        class="btn-gray">Edit</a>
+                                    <form method="POST" action="{{ route('prospects.destroy', $prospect->id) }}">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button onclick="return confirm('Are you sure')" class="ml-2 block btn-primary">
+                                            DELETE
+                                        </button>
 
-                        </tr>
+                                    </form>
+                                </td>
 
-                    @empty
-                        No data
-                    @endforelse
+                            </tr>
+
+                        @empty
+                            No data
+                        @endforelse
                     </tbody>
                 </table>
             </div>
@@ -79,10 +88,14 @@
     </div>
 
     <script>
-        $(document).ready(function () {
-            $('#dataTable').DataTable();
-
-        });
+        $(document).ready(function() {
+            $('#dataTable').DataTable({
+                "lengthMenu": [
+                    [10, 25, 50, -1],
+                    [10, 25, 50, "All"]
+                ]
+            });
+        })
     </script>
 
 </x-app-layout>
