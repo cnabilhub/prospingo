7a9f550bb21244bffe39aff7902952dd38aea7b6
Mouhsssine Nabil <46906074+cnabilhub@users.noreply.github.com>
2022-06-27 14:38:26 +0100

    ok

--- resources/views/prospects/index.blade.php
+++ resources/views/prospects/index.blade.php
@@ -5,6 +5,9 @@
         </h2>
     </x-slot>
     <div class="p-3">
+
+
+
         <div class="flex flex-col">
             <div id='recipients' class="p-8 mt-4 mb-2  lg:mt-0 rounded shadow bg-white">
 
@@ -41,7 +44,8 @@
                             <tr class="bg-white border-b">
 
                                 <td class="text-sm text-gray-900 font-light px-2 py-1 whitespace-nowrap">
-                                    {{ $prospect->site_url }}
+                                    <a href="https://{{ $prospect->site_url }}" class="text-pink-600"
+                                        target="_blank">{{ $prospect->site_url }}</a>
                                 </td>
 
                                 <td class="text-sm text-gray-900 font-light px-2 py-1 whitespace-nowrap">
@@ -50,11 +54,22 @@
 
 
                                 <td class="text-sm text-gray-900 font-light px-2 py-1 whitespace-nowrap">
-                                    {{ $prospect->email }}
+                                    <a href="mailto:{{ $prospect->email }}" class="text-pink-600">
+                                        {{ $prospect->email }}
+                                    </a>
                                 </td>
+                                @php
+                                    $tel = $prospect->telephone;
+                                    $tel = trim($tel);
+                                    $tel = str_replace(' ', '', $tel);
+                                    $tel = str_replace('+', '', $tel);
+                                @endphp
 
                                 <td class="text-sm text-gray-900 font-light px-2 py-1 whitespace-nowrap">
-                                    {{ $prospect->telephone }}
+                                    <a href="https://api.whatsapp.com/send?phone={{ $tel }}"
+                                        class="text-pink-600">
+                                        {{ $tel }}
+                                    </a>
                                 </td>
 
                                 <td class="text-sm text-gray-900 font-light px-2 py-1 whitespace-nowrap">
@@ -64,8 +79,7 @@
                                 </td>
 
                                 <td class=" text-sm text-gray-900 font-light px-2 py-1 whitespace-nowrap flex">
-                                    <a href="{{ route('prospects.edit', $prospect->id) }}"
-                                        class="btn-gray">Edit</a>
+                                    <a href="{{ route('prospects.edit', $prospect->id) }}" class="btn-gray">Edit</a>
                                     <form method="POST" action="{{ route('prospects.destroy', $prospect->id) }}">
                                         @csrf
                                         @method('DELETE')
